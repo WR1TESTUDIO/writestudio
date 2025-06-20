b4498e9ec87c367d3f7fa302e4b6e7b9864cf506
WR1TESTUDIO <kontakt.wr1te@gmail.com>
2025-06-20 20:23:42 +0200

    Update list-users.php

--- list-users.php
+++ list-users.php
@@ -1,4 +1,23 @@
 <?php
-$dir = __DIR__ . "/chat-data";
-$files = array_diff(scandir($dir), ['.', '..']);
-echo json_encode(array_values($files));
+$users = [];
+foreach (glob("chat-data/*.txt") as $file) {
+  $users[] = basename($file, ".txt");
+}
+header('Content-Type: application/json');
+echo json_encode($users);
+?>
+
+
+// add-user.php
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $username = trim($_POST['username'] ?? '');
+  if ($username !== '') {
+    $filepath = __DIR__ . "/chat-data/" . $username . ".txt";
+    if (!file_exists($filepath)) {
+      file_put_contents($filepath, "Witamy, $username!\n");
+    }
+  }
+  header("Location: index.html");
+}
+?>
