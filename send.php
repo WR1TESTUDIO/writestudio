c4d24518cc79fa574d0e2e9518bdda6098303d5f
WR1TESTUDIO <kontakt.wr1te@gmail.com>
2025-06-20 17:26:48 +0200

    Update send.php

--- send.php
+++ send.php
@@ -1,14 +1,16 @@
 <?php
 $data = json_decode(file_get_contents("php://input"), true);
-if (!$data || !isset($data["message"])) {
+if (!$data || !isset($data["message"]) || !isset($data["category"])) {
   http_response_code(400);
   echo "Błąd danych wejściowych.";
   exit;
 }
 
 $msg = htmlspecialchars($data["message"]);
-$subject = "Nowa wiadomość z chatu WR1TE";
-$headers = "From: WR1TE Chat <chat@wr1testudio.pl>";
+$category = strtolower(trim($data["category"]));
+$subject = "Nowa wiadomość z chatu WR1TE - Kategoria: " . ucfirst($category);
+$headers = "From: WR1TE Chat <chat@wr1testudio.pl>\r\n" .
+           "Content-Type: text/plain; charset=UTF-8\r\n";
 
 $dziedziny = [
   "tekst" => ["nates1ore@gmail.com", "sandrafieske@gmail.com", "szymon.szostak4@gmail.com", "amelia.music@wp.pl"],
@@ -19,15 +21,21 @@ $dziedziny = [
   "video" => ["lesser.kontakt@gmail.com"]
 ];
 
-// domyślnie wszyscy
-$emails = array_unique(array_merge(...array_values($dziedziny)));
+// Sprawdź czy kategoria istnieje i wybierz odpowiednie maile
+if (array_key_exists($category, $dziedziny)) {
+  $emails = $dziedziny[$category];
+} else {
+  // Jeśli kategoria nieznana, wyślij do wszystkich jako awaryjne rozwiązanie
+  $emails = array_unique(array_merge(...array_values($dziedziny)));
+}
 
+// Wyślij maila do każdego odbiorcy z danej kategorii
 foreach ($emails as $email) {
   mail($email, $subject, $msg, $headers);
 }
 
-// zapisz do pliku
-file_put_contents("chat.txt", "Użytkownik: $msg\n", FILE_APPEND);
+// Zapisz wiadomość do pliku (opcjonalnie)
+file_put_contents("chat.txt", "Kategoria: $category\nWiadomość: $msg\n\n", FILE_APPEND);
 
 echo "OK";
 ?>
